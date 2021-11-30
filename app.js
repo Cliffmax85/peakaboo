ef1588322031146887a46a351ac3f64c23e48641
cliffmax85 <cliffmax85@gmail.com>
2021-11-29 23:30:57 -0800

    updated hide and seek

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const boulderContainer = document.getElementById('boulder-container');
 
 const totalEl = document.getElementById('total');
 const winsEl = document.getElementById('wins');
+const lossesEl =document.getElementById('losses');
 
 // initialize state
 const hidingPlaces = [
@@ -28,36 +29,50 @@ function getRandomItem(arr) {
 
 function handleGuess(userGuess, correctSpot) {
     // should reset the styles
-
+    resetStyles();
     // then increment the guesses
-
+    totalGuesses++;
     // then grab the appropriate container element for the correct guess from the DOM
+    let correctEl = document.getElementById(`${correctSpot}-container`);
 
     // then add the face class to that element so that the face shows up
+    correctEl.classList.add('face');
 
     // then if the user guess is correct, increment the correct guesses
-
+    if (userGuess === correctSpot) {
+        correctGuesses++;
+    }
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
+    totalEl.textContent = totalGuesses;
+    winsEl.textContent = correctGuesses;
+    lossesEl.textContent = totalGuesses - correctGuesses;
+
 }
 
 function resetStyles() {
     // should remove the face class from all containers
+    shedContainer.classList.remove('face');
+    treeContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
 }
 
 shedButton.addEventListener('click', () => {
     // should get a random item to call the 'correct spot'
-
+    const randonButton = getRandomItem(hidingPlaces);
     // then use that correct spot to 'handle the guess' using the handleGuess function
+    handleGuess(`shed`, randonButton);
 });
 
 treeButton.addEventListener('click', () => {
     // should get a random item to call the 'correct spot'
-
+    const randonButton = getRandomItem(hidingPlaces);
     // then use that correct spot to 'handle the guess' using the handleGuess function
+    handleGuess(`tree`, randonButton);
 });
 
 boulderButton.addEventListener('click', () => {
     // should get a random item to call the 'correct spot'
-
+    const randonButton = getRandomItem(hidingPlaces);
     // then use that correct spot to 'handle the guess' using the handleGuess function
+    handleGuess(`shed`, randonButton);
 });
